bc3143f75092812039a22fb44424f63991edf3bf
user <user@Abhilash>
2023-08-03 19:53:17 -0400

    Demo committed

--- src/test/java/sample/Demo.java
+++ src/test/java/sample/Demo.java
@@ -1,5 +1,13 @@
 package sample;
 
-public class Demo {
+import org.testng.annotations.Test;
+
+public class Demo 
+{
+	@Test
+	public void sample()
+	{
+		System.out.println("hi this is first commit");
+	}
 
 }
